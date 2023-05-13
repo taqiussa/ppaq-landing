2c9e3300f1214e44e07d2e693f094760075b842d
unknown <taqiussa>
2023-05-13 14:34:51 +0700

    profil guru

--- src/data/guru-data.js
+++ src/data/guru-data.js
@@ -56,6 +56,13 @@ export const guruData = [
     ],
   },
   {
+    img: "/img/zazuk.jpeg",
+    name: "Zazuk Asiqoh,S.Pd.",
+    position: "Guru Mapel Pendidikan Agama Islam",
+    socials: [
+    ],
+  },
+  {
     img: "/img/yuni.jpeg",
     name: "Sri Wahyuni,S.Pd.",
     position: "Guru Mapel Pendidikan Pancasila dan Kewarganegaraan",
@@ -69,6 +76,357 @@ export const guruData = [
     socials: [
     ],
   },
+  {
+    img: "/img/anis.jpeg",
+    name: "Anis Fitria,S.Pd.",
+    position: "Guru Mapel Bahasa Indonesia",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/hanif.jpeg",
+    name: "Hanif Yulakha,S.Pd.",
+    position: "Guru Mapel Bahasa Indonesia",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/ib.jpeg",
+    name: "Ib'rotul Khasanah,S.Pd.",
+    position: "Guru Mapel Bahasa Indonesia",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/niswa.jpeg",
+    name: "Niswatun Khasanah,S.Pd.",
+    position: "Guru Mapel Bahasa Indonesia",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/adiba.jpeg",
+    name: "Adibatun Nisa',S.Pd.",
+    position: "Guru Mapel Bahasa Indonesia",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/galuh.jpeg",
+    name: "Galuh Bestari Restu Maya Pada,S.Pd.",
+    position: "Guru Mapel Bahasa Inggris",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/tomy.jpeg",
+    name: "Tomy Widiyanto,M.Pd.Gr.",
+    position: "Guru Mapel Bahasa Inggris",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/mukhid.jpeg",
+    name: "Abdul Mukhid,S.Pd.",
+    position: "Guru Mapel Bahasa Inggris",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/mukhid.jpeg",
+    name: "Abdul Mukhid,S.Pd.",
+    position: "Guru Mapel Bahasa Inggris",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/desi.jpeg",
+    name: "Desi Hidayati,S.Pd.",
+    position: "Guru Mapel IPA",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/dewi.jpeg",
+    name: "Dewi Khariroh,S.Pd.",
+    position: "Guru Mapel IPA",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/reni.jpeg",
+    name: "Reni Setyowati,S.Pd.",
+    position: "Guru Mapel IPA",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/noris.jpeg",
+    name: "Norisma Rizky A,S.Pd.",
+    position: "Guru Mapel IPS",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/rizky.jpeg",
+    name: "Rizky Purwati,S.Pd.",
+    position: "Guru Mapel Matematika",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/fitri.jpeg",
+    name: "Fitri Nur Oktiviani,S.Pd.",
+    position: "Guru Mapel Matematika",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/umi.jpeg",
+    name: "Umi Fahruroh,S.Pd.",
+    position: "Guru Mapel Matematika",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/risca.jpeg",
+    name: "Risca Sylviana,S.Pd.",
+    position: "Guru Mapel Matematika",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/elis.jpeg",
+    name: "Rizka Elistanti,S.Pd.",
+    position: "Guru Mapel Pendidikan, Jasmani dan Olahraga",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/arif.jpeg",
+    name: "Moh Arif Rahman H.,S.Pd.",
+    position: "Guru Mapel Pendidikan, Jasmani dan Olahraga",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/dhani.jpeg",
+    name: "M Setyaki Kurnia Ramadhani.,S.Pd.",
+    position: "Guru Mapel Pendidikan, Jasmani dan Olahraga",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/agus.jpeg",
+    name: "Moh Agus Mutohir,S.Pd.",
+    position: "Guru Mapel Akidah",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/adib.jpeg",
+    name: "Moh Adib Imron,M.Pd.",
+    position: "Guru Mapel Akidah",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/fairuz.jpeg",
+    name: "Fairuz",
+    position: "Guru Mapel Tarikh",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/fatah.jpeg",
+    name: "Ina Fatahna,S.Ag.",
+    position: "Guru Mapel Seni Budaya",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/umam.jpeg",
+    name: "M Khoirul Umam,S.Ag.",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/ibnu.jpeg",
+    name: "M Ibnu Mas'ud,S.Pd.I.",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/hadi.jpeg",
+    name: "Ahmad Nur Hadi",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/wakhid.jpeg",
+    name: "M Luqman Fitroni Nor Wakhid",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/isyfaq.jpeg",
+    name: "Isyfaq Hidayat",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/subiyat.jpeg",
+    name: "Subiyat,S.Pd.",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/subiyat.jpeg",
+    name: "Subiyat,S.Pd.",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/subiyat.jpeg",
+    name: "Subiyat,S.Pd.",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/muja.jpeg",
+    name: "Mujaidin",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/teguh.jpeg",
+    name: "Teguh Riyadi",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/yulianto.jpeg",
+    name: "Mokhamad Yulianto,S.Pd.",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/islah.jpeg",
+    name: "Muhammad Islah,S.Pd.",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/zen.jpeg",
+    name: "Zaenuri,S.Pd.",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/syahidin.jpeg",
+    name: "Ahmad Syahidin,S.Pd.",
+    position: "Guru Al Qur'an",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/bed.jpeg",
+    name: "Ahmad Zubaedi,S.Kom",
+    position: "Guru Mapel Informatika",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/taqi.jpeg",
+    name: "Taqius Shofi Albastomi,S.Kom",
+    position: "Guru Mapel Informatika",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/doni.jpeg",
+    name: "Doni Soenarko,S.Pd.",
+    position: "Guru Bimbingan dan Konseling",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/hafidz.jpeg",
+    name: "M Hafidz Habiburohman,M.A.",
+    position: "Guru Bimbingan dan Konseling",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/ria.jpeg",
+    name: "Ria Lestari,S.Pd.",
+    position: "Guru Bimbingan dan Konseling",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/imut.jpeg",
+    name: "Siti Mutmainah,S.Pd.",
+    position: "Guru Bimbingan dan Konseling",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/imam.jpeg",
+    name: "Imam Turmudzi,S.E",
+    position: "Kepala Tata Usaha",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/ismi.jpeg",
+    name: "Ismi Sholehatul Adawiyah,S.E",
+    position: "Bendahara Sekolah",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/uswatun.jpeg",
+    name: "Uswatun Khasanah,S.Pd.",
+    position: "Bendahara BOS",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/roni.jpeg",
+    name: "M Khaeroni Alamul Huda,S.Ag.",
+    position: "Operator Sekolah",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/izza.jpeg",
+    name: "Muh Aizza A'ista,S.Ag.",
+    position: "Tata Usaha",
+    socials: [
+    ],
+  },
+  {
+    img: "/img/syahrul.jpeg",
+    name: "Syahrul Gufron",
+    position: "Tata Usaha",
+    socials: [
+    ],
+  },
+
 ];
 
 export default guruData;
