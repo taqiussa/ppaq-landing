e2f3333610915cad07d402a12d1c771ec42a362e
unknown <taqiussa>
2023-05-13 07:25:20 +0700

    navbar

--- src/widgets/layout/navbar.jsx
+++ src/widgets/layout/navbar.jsx
@@ -70,14 +70,6 @@ export function Navbar({ brandName, routes, action }) {
         </Link>
         <div className="hidden lg:block">{navList}</div>
         <div className="hidden gap-2 lg:flex">
-          <a
-            href="https://www.material-tailwind.com/blocks?ref=mtkr"
-            target="_blank"
-          >
-            <Button variant="text" size="sm" color="white" fullWidth>
-              pro version
-            </Button>
-          </a>
           {React.cloneElement(action, {
             className: "hidden lg:inline-block",
           })}
@@ -102,15 +94,6 @@ export function Navbar({ brandName, routes, action }) {
       >
         <div className="container mx-auto">
           {navList}
-          <a
-            href="https://www.material-tailwind.com/blocks/react?ref=mtkr"
-            target="_blank"
-            className="mb-2 block"
-          >
-            <Button variant="text" size="sm" fullWidth>
-              pro version
-            </Button>
-          </a>
           {React.cloneElement(action, {
             className: "w-full block",
           })}
@@ -121,14 +104,14 @@ export function Navbar({ brandName, routes, action }) {
 }
 
 Navbar.defaultProps = {
-  brandName: "Material Tailwind React",
+  brandName: "SMP Al Musyaffa",
   action: (
     <a
-      href="https://www.creative-tim.com/product/material-tailwind-kit-react"
+      href="https://siakad.smpalmusyaffa.com"
       target="_blank"
     >
       <Button variant="gradient" size="sm" fullWidth>
-        free download
+        siakad
       </Button>
     </a>
   ),
