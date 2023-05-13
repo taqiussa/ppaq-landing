eb11c033b02a5dffb3253cb64d924082fef14d3b
taqiussa <taqiussa@gmail.com>
2023-05-13 10:30:51 +0700

    features data

--- src/data/features-data.js
+++ src/data/features-data.js
@@ -1,28 +1,28 @@
 import {
   StarIcon,
-  ArrowPathIcon,
-  FingerPrintIcon,
+  AcademicCapIcon,
+  TrophyIcon,
 } from "@heroicons/react/24/solid";
 
 export const featuresData = [
   {
     color: "blue",
-    title: "Awarded Agency",
-    icon: StarIcon,
+    title: "Berilmu",
+    icon: AcademicCapIcon,
     description:
       "Divide details about your product or agency work into parts. A paragraph describing a feature will be enough.",
   },
   {
-    color: "red",
-    title: "Free Revisions",
-    icon: ArrowPathIcon,
+    color: "orange",
+    title: "Religius",
+    icon: StarIcon,
     description:
       "Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious.",
   },
   {
     color: "teal",
-    title: "Verified Company",
-    icon: FingerPrintIcon,
+    title: "Berprestasi",
+    icon: TrophyIcon,
     description:
       "Write a few lines about each one. A paragraph describing a feature will be enough. Keep you user engaged!",
   },
