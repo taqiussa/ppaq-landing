9df3b2dc2b1ff66b578ad0e64704a6e76fe1d2ef
taqiussa <taqiussa@gmail.com>
2023-05-18 19:30:48 +0700

    pt profil guru

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,10 +1,5 @@
-import { Avatar, Typography, Button } from "@material-tailwind/react";
+import { Avatar, Typography } from "@material-tailwind/react";
 import React from "react";
-import {
-  MapPinIcon,
-  BriefcaseIcon,
-  BuildingLibraryIcon,
-} from "@heroicons/react/24/solid";
 import { Footer } from "@/widgets/layout";
 import { GuruCard } from "@/widgets/cards";
 import { guruData } from "@/data";
@@ -17,7 +12,7 @@ export function Profile() {
         <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
       </section>
       <section className="relative bg-blue-gray-50/50 py-16 px-4">
-        <div className="container mx-auto">
+        <div className="container mx-auto pt-20">
           <div className="relative mb-6 -mt-64 flex w-full min-w-0 flex-col break-words rounded-3xl bg-white shadow-xl shadow-gray-500/5">
             <div className="px-6">
               <div className="flex flex-wrap justify-center">
